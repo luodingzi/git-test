ebca668947f6f198680f253860a4f86204f2df09
luodingzi <luodingzi@gmail.com>
2017-06-23 11:41:29 +0800

    connect database

--- index.php
+++ index.php
@@ -5,7 +5,21 @@ $mysqli = new mysqli("localhost","root","RootAdmin20170613!","test");
 if(!$mysqli){
   echo "databae error";
    }else{
-	echo "successfule";
+	echo "connect successful";
 }
+
+echo "<hr />";
+
+$sql = "select * from goods";
+
+$result = $mysqli->query($sql);
+
+var_dump($result);
+
+
+
+
+
 $mysqli->close();
+
 ?>
